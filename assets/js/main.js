2c6c15b26be6ae404be1948c7f3b738f146c8650
Eveline-97 <57260992+Eveline-97@users.noreply.github.com>
2022-04-08 16:29:11 +0200

    new logs

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,12 @@ for (let i = 0; i < aList.length; i++) {
     )
 }
 
+let repetitions = [];
+for (let i = 0; i < aList.length; i++) {
+    repetitions.push(0)
+}
+console.log(repetitions);
+
 //init synths
 const synth = new Tone.PolySynth(Tone.Synth).toDestination();
 
@@ -24,6 +30,12 @@ for (let i = 0; i < aList.length; i++) {
             console.log(releases[a.id]);
             synth.triggerRelease(releases[a.id]);
         } else {
+            //add repetitions
+            console.log(repetitions[i]);
+            repetitions[i]++;
+            let rep = document.getElementById(`repeats${i}`);
+            rep.innerHTML = repetitions[i];
+
             //schwebung in positioning
             let left = Math.random()*10.;
             let top = Math.random()*10.;
@@ -60,132 +72,7 @@ for (let i = 0; i < aList.length; i++) {
             }
         }
     });
-    /*else {
-        //(re-)adjust size to basic values
-        a.style.width = "25px";
-        a.style.heigth = "25px";
-    }*/
-}
-
-//logs
-let currentNote = document.getElementById("current-note");
-let repeatP = document.getElementById("repeats");
-let rowP = document.getElementById('current-row');
-currentNote.innerHTML = `Current note: ${current + 1}`;
-repeatP.innerHTML = `Repeats: ${repeat - 1}`;
-rowP.innerHTML = `Row: ${row + 1}`;
-
-let speedSlider = document.getElementById('speed');
-let speedP = document.getElementById('speed-amount');
-let speed = speedSlider.value;
-
-const toggleStart = (button) => {
-    if (button.innerHTML == 'start') {
-        button.innerHTML = 'pause';
-    } else {
-        button.innerHTML = 'start';
-    }
-}
-
-const play = () => {
-    toggleStart(start);
-    if (start.innerHTML == 'pause') {
-        this.interval = setInterval(loop, speed);
-    } else {
-        clearInterval(this.interval);
-    }
-
-    speedSlider.onchange = () => {
-        speed = speedSlider.value;
-        speedP.innerHTML = `${speedSlider.value}ms between notes`;
-        clearInterval(this.interval);
-    
-        if (start.innerHTML == 'pause') {
-            this.interval = setInterval(loop, speed);
-        }
-    }
 }
 
 //start
 Tone.start();
-start.addEventListener('click', play);
-
-/***-TITLE-***/
-/*with keyframes.js*/
-let titleChildren = document.getElementById('title').children;
-
-for (let i = 0; i < titleChildren.length; i++) {
-    let deg = Math.random() * 30 - 15;
-    let seconds = Math.random() * 4 + 1; //1-5 seconds
-    Keyframes.define([
-        {
-            name: `schwebung${i}`,
-            '0%': {transform: `rotate(0deg)`},
-            '25%': {transform: `rotate(-${deg}deg)`},
-            '75%': {transform: `rotate(${deg}deg)`},
-            '100%': {transform: `rotate(0deg)`}
-        }
-    ]);
-    let letter = new Keyframes(document.getElementById(`letter${i}`));
-    letter.play(`schwebung${i} ${seconds}s linear infinite`);
-}
-
-/***-ROWS-***/
-let rows = document.getElementsByClassName('row');
-let rowAmount = document.getElementById('row-amount');
-let add = document.getElementById('add-row');
-let remove = document.getElementById('remove-row');
-
-const addRow = () => {
-    let lastRow = document.getElementById(`row-${rows.length}`);
-    //create new row
-    let el = document.createElement('div');
-    el.id = `row-${rows.length+1}`;
-    el.classList.add('row');
-
-    //children of the last row that are to become the children of the new element
-    children = [
-        lastRow.children[1],
-        lastRow.children[2],
-        lastRow.children[3],
-        lastRow.children[4],
-        lastRow.children[5],
-        lastRow.children[6],
-        lastRow.children[7],
-        lastRow.children[8],
-        lastRow.children[9],
-        lastRow.children[2]
-    ];
-
-    //loop over children and add to element
-    for (let i = 0; i < children.length; i++) {
-        let child = document.createElement('p');
-        child.classList = children[i].classList;
-        el.appendChild(child);
-    }
-    lastRow.insertAdjacentElement('afterend', el);
-
-    //update amount of rows
-    rowAmount.innerHTML = `Rows: ${rows.length}`;
-}
-
-const removeRow = () => {
-    let lastRow = document.getElementById(`row-${rows.length}`);
-    if (rows.length > 1) {
-        if (rows.length == row + 1) {
-            alert(`The row is currently playing and can not be removed.`);
-            return;
-        } else {
-            lastRow.remove();
-        }
-    } else {
-        alert(`The first row can not be removed.`);
-        return;
-    }
-
-    //update amount of rows
-    rowAmount.innerHTML = `Rows: ${rows.length}`;
-}
-
-add.addEventListener('click', addRow);
-remove.addEventListener('click', removeRow);
